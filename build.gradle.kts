c90a8d84e9e95297b50ecd9be34c5f14b39db52c
星河旧梦 <65857043+3582730951@users.noreply.github.com>
2024-11-18 15:34:43 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ cmaker {
                 "-DANDROID_STL=none",
             )
         )
-        abiFilters("arm64-v8a", "x86_64", "riscv64")
+        abiFilters("arm64-v8a")
     }
     buildTypes {
         if (it.name == "release") {
